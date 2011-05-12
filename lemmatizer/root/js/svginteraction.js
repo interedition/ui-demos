95d1c0c41f04de397cd70cb454ed2a5f85e0e53f
Joris van Zundert <joris.van.zundert@gmail.com>
2011-05-12 02:11:57 +0200

    Finishe OO'ing. Still some duplication in svgLoaded() and Node_obj.dblClick_listener.

--- lemmatizer/root/js/svginteraction.js
+++ lemmatizer/root/js/svginteraction.js
@@ -1,54 +1,20 @@
 function svgLoaded() {
-  $('.node').dblclick( function(evt) {
-    node_target = $(evt.target);
-    node_id = node_target.siblings('title').text();
-    change_node_state(node_id);
-  }).children('ellipse').each( function() {
-    $(this).attr( {stroke:'black', fill:'#fff'} );
-  });
-  change_node_state( null, add_node_objs );
-  $('#log').ajaxError(function() {
-    $(this).text( 'Oops.. something went wrong with trying to save this change. Please try again...' );
-  });
-}
-
-// We're assuming JSON of the form:
-//   { 'node_1': 1, 'node_2': 0, 'node_3': 0, 'node_4': null, 'node_5': 1 }
-// with the following heuristics:
-//   1 -> turn the associated SVG node on, put in the associate word in the text box.
-//   0 -> turn SVG node off.
-//   null -> turn node off, put in ellipsis in text box at the corresponding place.
-
-function change_node_state(node_id, callback) {
-  var jqjson = $.getJSON( 'nodeclick', 'node_id=' + node_id, function(data) {
-    $('#constructedtext').empty();
-    $.each( data, function(item, value) {
-      node_id = value[0];
-      state = value[1];
-      node_title = $('.node').children('title').filter( function(index) {
-        return $(this).text() == node_id;
-      });
-      node_text = node_title.siblings('text').text();
-      node_ellipse = node_title.siblings('ellipse');
-      if( state == 1 ) {
-        // node_ellipse.css( 'fill', '#b3f36d' );
-        // node_ellipse.css( { 'fill':'#b3f36d', 'stroke':'green' } );
-        // node_ellipse.style.fill = 'green';
-        // Above solutions don't work with FF3.6 (most do in FF4 btw)
-        // Apparently the svg is not fully part of the DOM in every sense in FF3?
-        // Maybe using JQuery::SVG would offer solutions in svg.style etc. but it seems overkill tbh
+  $('ellipse').attr( {stroke:'black', fill:'#fff'} );
+  var jqjson = $.getJSON( 'nodeclick', 'node_id=null', function(data) {
+    $.each( data, function(item, node_id_and_state) {
+      if( node_id_and_state[1] == 1 ) {
+        node_ellipse = $('.node').children('title').filter( function(index) {
+          return $(this).text() == node_id_and_state[0];
+        }).siblings('ellipse');
         node_ellipse.attr( {stroke:'green', fill:'#b3f36d'} );
-        if( node_ellipse.data( 'node_obj' ) ) { node_ellipse.data( 'node_obj' ).draggable( false ) };
-        $('#constructedtext').append( node_text + '&#32;' );
+        $('#constructedtext').append( node_ellipse.siblings('text').text() + '&#32;' );
       } else {
-        node_ellipse.attr( {stroke:'black', fill:'#fff'} );
-        if( node_ellipse.data( 'node_obj' ) ) { node_ellipse.data( 'node_obj' ).draggable( true ) };
-        if( state == null ) {
+        if( node_id_and_state[1] == null ) {
           $('#constructedtext').append( ' &hellip; ' );
         }
       }
     });
-    if( callback ) { callback() };
+    add_node_objs();
   });
 }
 
@@ -68,6 +34,41 @@ function node_obj(ellipse) {
   this.dx = 0;
   this.dy = 0;
   this.node_elements = node_elements_for(self.ellipse);
+
+  this.dblclick_listener = function(evt) {
+    node_id = ellipse.siblings('title').text();
+    var jqjson = $.getJSON( 'nodeclick', 'node_id=' + node_id, function(data) {
+      $('#constructedtext').empty();
+      $.each( data, function(item, node_id_and_state) {
+        node_ellipse = $('.node').children('title').filter( function(index) {
+          return $(this).text() == node_id_and_state[0];
+        }).siblings('ellipse');
+        // 1 -> turn the associated SVG node on, put in the associate word in the text box.
+        // 0 -> turn SVG node off.
+        // null -> turn node off, put in ellipsis in text box at the corresponding place.
+        if( node_id_and_state[1] == 1 ) {
+          $('#constructedtext').append( node_ellipse.siblings('text').text() + '&#32;' );
+          if( node_ellipse.data( 'node_obj' ) ) { node_ellipse.data( 'node_obj' ).set_draggable( false ) }
+        } else {
+          if( node_ellipse.data( 'node_obj' ) ) { node_ellipse.data( 'node_obj' ).set_draggable( true ) };
+          if( node_id_and_state[1] == null ) {
+            $('#constructedtext').append( ' &hellip; ' );
+          }
+        }
+      });
+    });
+  }
+
+  this.set_draggable = function( draggable ) {
+    if( draggable ) {
+      self.ellipse.attr( {stroke:'black', fill:'#fff'} );
+      self.ellipse.mousedown( this.mousedown_listener );
+      self.ellipse.hover( this.enter_node, this.leave_node );  
+    } else {
+      self.ellipse.unbind('mouseenter').unbind('mouseleave').unbind('mousedown');
+      self.ellipse.attr( {stroke:'green', fill:'#b3f36d'} );
+    }
+  }
 
   this.mousedown_listener = function(evt) {
     evt.stopPropagation();
@@ -113,16 +114,8 @@ function node_obj(ellipse) {
     });
   }
 
-  this.draggable = function( draggable ) {
-    if( draggable ) {
-      this.ellipse.mousedown( this.mousedown_listener );
-      this.ellipse.hover( this.enter_node, this.leave_node );  
-    } else {
-      this.ellipse.unbind('mouseenter').unbind('mouseleave').unbind('mousedown');
-    }
-  }
-
-  self.draggable( true );
+  this.ellipse.dblclick( this.dblclick_listener );
+  self.set_draggable( true );
 }
 
 function svgshape( shape_element ) {
@@ -176,7 +169,10 @@ function get_edge_elements_for( ellipse ) {
   return edge_elements;
 } 
 
-$(document).ready(function () {        
+$(document).ready(function () {
+  $('#log').ajaxError(function() {
+    $(this).text( 'Oops.. something went wrong with trying to save this change. Please try again...' );
+  });
   $('#graph').mousedown(function (event) {
     $(this)
       .data('down', true)
