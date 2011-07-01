460f3fafb96541c87110c884e9b77fa4d93ad900
Joris van Zundert <joris.van.zundert@gmail.com>
2011-07-01 17:03:52 +0200

    Working in interpreting JSON for column 2.

--- appengine/js/collatex.js
+++ appengine/js/collatex.js
@@ -1,64 +1,18 @@
 
-	var curLine=1;
 	var curUrl=1;
-	var fileContents=[];
-	var firstResponse = [];
-	function saveName(fn,winName){
-        
-		$("#"+winName).parent().append("<span class='fileName' id="+winName+"'>"+fn+"</span>");
-		$("#"+winName).hide();
-	}
-	function removeFile(fileRemove){
-		$(fileRemove).parent().remove();
-		
-	}
+
 	function removeUrl(fileRemove){
 		$(fileRemove).parent().remove();
 		
 	}
-	function showContents(me){
-		if (me.contentWindow.location.href.substring(me.contentWindow.location.href.lastIndexOf("/")+1) != "fileFrame.html") {
-			//for (n in me.contentWindow.document){
-				var de=me.contentWindow.document.documentElement;
 
-				var wname = me.name;				
-				if (de.nodeName.substring(0,3).toLowerCase()=="tei"){
-					fileContents.push({"name":wname,"file":$(me).next().text(),"text":"<"+de.nodeName+">"+de.innerHTML+"</"+de.nodeName+">"});
-				}
-				else{
-					fileContents.push({"name":wname,"file":$(me).next().text(),"text":de.getElementsByTagName("body")[0].innerHTML});
-				}
-		}
-	}
-	function newFileLine(){
-	
-	fileLine = "<div class='fileLine'><span class='fileBox'><iframe frameborder=0 name='file"+curLine+"' id='file"+curLine+"' class='fileFrame' onload='showContents(this)' src='/frames/fileFrame.html'> </iframe></span><span onclick='removeFile(this)' class='divLink'>Remove</span></div>";
-	$("#allFiles").append($(fileLine));
-	curLine++;
-	return;
-	}
 	function newUrl(){
 		var urlLine = "<div><input type='text' name='url"+curUrl+"' id='url"+curUrl+"' class='textfieldX'/><div id='removeUrl' onclick='removeUrl(this)' class='divLink ui-button-icon-primary ui-icon ui-icon-cancel' style='float: right; margin-right: 15px; margin-top:2px;'></div></div>";
 		$("#allUrls").append($(urlLine));
 		curUrl++;
 	}
-	function showSubmitted(){
-		
-	}
-	function parseString(){
-		// From http://www.w3schools.com/Xml/xml_parser.asp
-		if (window.DOMParser) {
-			parser = new DOMParser();
-			xmlDoc = parser.parseFromString(txt, "text/xml");
-		}
-		else // Internet Explorer
-		{
-			xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-			xmlDoc.async = "false";
-			xmlDoc.loadXML(txt);
-		}
-	}
-        function listTexts(name,val,txtjson,targetId){
+
+    function listTexts(name,val,txtjson,targetId){
 			
 	        var sig = txtjson["autosigil"];
 		if (!sig) {
@@ -77,51 +31,35 @@
 		}
 		
 	}
+
+	// {"corpus.xml": [{"text": "6N_U0J3vhnQ4IUv_gkGCDw==-0", 
+	//              "autosigil": "B", "parent": null}, {"text": 
+	//              "6N_U0J3vhnQ4IUv_gkGCDw==-1", "autosigil": "C", 
+	//              "parent": null}], "group.xml": [{"text": 
+	//              "2w_FiYkh7SJuBIEph_W0Ug==-0", "autosigil": "D", 
+	//              "parent": null}], "plain.txt": [{"text": 
+	//              "3Vo0CVvDyqd9V7wTzGIeCQ==-0", "autosigil": "A", 
+	//              "parent": null}]}
+	// $("#"+targetId).append("<li id='li_"+name+"'><input type='checkbox' checked='true' value='"+name+"'
+	//  name='sel_li_"+name+"' id='sel_li_"+name+"'></input>
+	// <input type='text' class='textfieldX sigil' name='"+name+"_sigil'
+	//  id='"+name+"_sigil' value='"+sig+"'/><span class='text_name'>"+val+"</span></li>");
+	
 	function submitForm(){
-		var allData="";
-		var uriNames =[];
-		var uriVals = [];
-		$.each($(".textfieldX"),function(index,value){
-			if ($(value).val().length > 0) {
-			
-				allData = allData + $(value).attr("name") + "=" + $(value).val() + "&";
-				uriNames.push($(value).attr("name"));
-				uriVals.push($(value).val());
-			}
-		});
-		for (var i=0;i<fileContents.length;i++){
-			var allData = allData+fileContents[i].name+"="+fileContents[i].text+"&";
-		}
-			
-		allData = allData.substring(0,allData.length-1);
-		
-		$.ajax({
-			url: "/return_texts",
-			type: "GET",
-			success: function(response){
-				firstResponse = response;
-				
-				$("#submittedFileList").html("");
-				if ((uriNames.length > 0) || (fileContents.length > 0)) {
-					$("#submitted_div").show();
-					if (uriNames.length > 0) {
-						for (var i = 0; i < uriNames.length; i++) {
-							for (var j = 0; j < response["" + uriNames[i]].length; j++) {
-							    listTexts(uriNames[i]+"_"+j, uriVals[i]+" "+j+" ", response["" + uriNames[i]][j], "submittedFileList");
-							}
-						}
-					}
-					if (fileContents.length > 0) {
-						for (var i = 0; i < fileContents.length; i++) {
-							for (var j = 0; j < response["" + fileContents[i].name].length; j++) {
-								listTexts(fileContents[i].name+"_"+j, fileContents[i].file+" "+j+" ", response["" + fileContents[i].name][j], "submittedFileList");
-							}
-						}
-					}
-				}
-			}
+		urls = $('#choosefile').serialize();
+		$.getJSON('/return_texts', urls, function(data){
+			$('#submittedFileList').html('');
+			if( data ) {$('#submitted_div').show();}
+			$.each(data, function(key, val) {
+				$.each(val, function(keyy, vall) {
+					console.log( keyy + ">>" + vall );
+				}); 
+				$('#submittedFileList').append( '<li class="uploaded_file_name">' + key + '</li>' );
+				console.log( key + ":" + val );
+			});
 		});
 	}
+	
 	function getTokens(){
 		fuzzymatch = $("#fuzzymatch").is(':checked');
 		collator = $("#collatorUL input:radio:checked").val();
@@ -166,12 +104,7 @@
 		
 	}
 	$(document).ready(function(e){
-		
-		newFileLine();
 		newUrl();
-		$("#addAnother").click(function(e){
-			newFileLine();
-		});
 		$("#addAnotherUrl").click(function(e){
 			newUrl();
 		});
