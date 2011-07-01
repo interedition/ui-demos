260dfbb6f0ca3e674eac2bb6835e5924661b7465
Tara L Andrews <tla@mit.edu>
2011-07-01 13:52:45 +0200

    parse texts from uploaded files, return JSON with their info

--- appengine/fileupload.py
+++ appengine/fileupload.py
@@ -1,15 +1,17 @@
+import handleInput
 import logging
 import simplejson as json
 import urllib
 from google.appengine.api import users
 from google.appengine.ext import blobstore
-from google.appengine.ext.blobstore import BlobKey
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 def GetUIData( blob_info ):
+    '''Return information about the file blob in a form expected by the
+    jQuery UI.'''
     data_struct = { 'name': blob_info.filename,
                     'size': blob_info.size,
                     'url': "/serve/%s" % blob_info.filename,
@@ -18,10 +20,20 @@ def GetUIData( blob_info ):
                     }
     return data_struct
 
-class BlobOwner( db.Model ):
-    user = db.UserProperty()
-    blobkey = db.StringProperty()
+#### Datastore classes
+class FileInfo( db.Model ):
+    blobkey = db.StringProperty()  # file ID: key
+    user = db.UserProperty()       # who uploaded the thing
+    type = db.StringProperty()     # plaintxt, teixml, collatexinput, etc.
 
+class FileText( db.Model ):
+    blobkey = db.StringProperty()  # file ID
+    id = db.StringProperty()       # unique text ID
+    textparent = db.StringProperty()   # parent text of this subtext
+    offset = db.IntegerProperty()  # where in the file I start
+    length = db.IntegerProperty()  # how long I am
+
+#### Web request handler classes
 class UploadURLHandler( webapp.RequestHandler ):
     def get( self ):
         upload_url = blobstore.create_upload_url( '/fileupload' )
@@ -31,7 +43,7 @@ class UploadURLHandler( webapp.RequestHandler ):
 class FileUploadHandler( blobstore_handlers.BlobstoreUploadHandler ):
     def get( self ):
         '''Return information on file blobs that are here'''
-        owner_files = db.GqlQuery( "SELECT * FROM BlobOwner WHERE user = USER('%s')" % users.get_current_user() )
+        owner_files = db.GqlQuery( "SELECT * FROM FileInfo WHERE user = USER('%s')" % users.get_current_user() )
         answer = []
         for file in owner_files:
             answer.append( GetUIData( blobstore.BlobInfo.get( file.blobkey ) ) )
@@ -39,14 +51,32 @@ class FileUploadHandler( blobstore_handlers.BlobstoreUploadHandler ):
         self.response.out.write( json.dumps( answer, ensure_ascii=False ).encode( 'utf-8' ) )
     
     def post( self ):
-        '''Handle the upload of file blobs'''
+        '''Handle the upload of file blobs, record the user to whom the blob
+        belongs, and find the constituent texts in the blob'''
         ## self.request has a reference to the blob(s) uploaded.
         uploaded_files = self.get_uploads('files[]')
         logging.info( 'Have %d uploaded files' % len( uploaded_files ) )
         query_files = []
         for blob_info in uploaded_files:
+            # Parse out the texts from the blob.
+            reader = blobstore.BlobReader( blob_info )
+            ### BIG TODO exception handling!
+            file_texts = handleInput.parse_file( reader.read() )
+            file_type = None
+            for text in file_texts:
+                logging.info( "Found text %s in file %s at offset %s, length %s" 
+                               % ( text['id'], blob_info.filename, text['offset'], text['length'] ) )
+                ft = FileText( blobkey = str( blob_info.key() ),
+                               id = text['id'],
+                               offset = text['offset'],
+                               length = text['length'] )
+                if 'parent' in text:
+                    ft.textparent = text['parent']
+                file_type = text['type']    # This can be taken from any record
+                ft.put()
             # Associate the blob with the user who uploaded it
-            b = BlobOwner( user = users.get_current_user(),
+            b = FileInfo( user = users.get_current_user(),
+                           type = file_type,
                            blobkey = str( blob_info.key() ) )
             # Push the blob key onto the query string for the JSON response
             b.put()
@@ -66,7 +96,7 @@ class UploadJSONResponse( webapp.RequestHandler ):
         for blob_key in blob_keys:
             answer.append( GetUIData( blobstore.BlobInfo.get( str( urllib.unquote( blob_key ) ) ) ) )
         self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write( json.dumps( answer, ensure_ascii=False ).encode( 'utf-8' ) )  # TODO json encode this
+        self.response.out.write( json.dumps( answer, ensure_ascii=False ).encode( 'utf-8' ) )
 
 class ServeHandler( blobstore_handlers.BlobstoreDownloadHandler ):
     def get( self, resource ):
@@ -79,14 +109,51 @@ class DeleteHandler( webapp.RequestHandler ):
         resource = str(urllib.unquote(resource))
         blob_info = blobstore.BlobInfo.get(resource)
         blob_info.delete()
-        this_blob_owner = db.GqlQuery( "SELECT * FROM BlobOwner WHERE blobkey = '%s'" % resource )
-        for b in this_blob_owner:
+        this_file_info = db.GqlQuery( "SELECT * FROM FileInfo WHERE blobkey = '%s'" % resource )
+        for b in this_file_info:
             db.delete( b )
+        this_file_texts = db.GqlQuery( "SELECT * FROM FileText WHERE blobkey = '%s'" % resource )
+        for t in this_file_texts:
+            db.delete( t )
         answer = []
         answer.append( "Deleted %s" % resource )
         self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write( json.dumps( answer, ensure_ascii=False ).encode( 'utf-8' ) )  # TODO json encode this
+        self.response.out.write( json.dumps( answer, ensure_ascii=False ).encode( 'utf-8' ) )
 
+class ReturnTexts( webapp.RequestHandler ):
+    def post( self ):
+        return get( self )
+
+    def get( self ):
+        '''Return a JSON response that is the list of texts and default sigla
+        that occur in the files uploaded by the user.'''
+        answer = {}
+        owner_files = db.GqlQuery( "SELECT * FROM FileInfo WHERE user = USER('%s')" % users.get_current_user() )
+        sequence = 0
+        for file in owner_files:
+            fileblob = blobstore.BlobInfo.get( file.blobkey )  # use this to get filename
+            logging.info( "Retrieving text(s) in file %s" % fileblob.filename )
+            answer[fileblob.filename] = []
+            stored_texts = db.GqlQuery( "SELECT * FROM FileText WHERE blobkey = '%s' ORDER BY id" % file.blobkey )
+            for st in stored_texts:
+                logging.info( "...found text %s for file %s" % ( st.id, fileblob.filename ) )
+                answer[fileblob.filename].append( { 'text': file.blobkey + '-' + st.id,
+                                                    'parent': st.textparent,
+                                                    'autosigil': self.autosigil( file.type, st.id, sequence ) } )
+                sequence += 1
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write( json.dumps( answer, ensure_ascii=False ).encode( 'utf-8' ) )
+    
+    def autosigil( self, filename, textid, autosig_sequence ):
+        '''Assigns a default sigil for a text, depending on its order and its
+        type.'''
+        sigil = None
+        if type == 'collatexinput':
+            sigil = textid
+        else:
+            sigil = chr( autosig_sequence + 65 )
+            autosig_sequence += 1
+        return sigil
 
 application = webapp.WSGIApplication(
                                      [
@@ -95,6 +162,7 @@ application = webapp.WSGIApplication(
                                       ('/serve/([^/]+)?', ServeHandler),
                                       ('/delete/([^/]+)?', DeleteHandler),
                                       ('/uploadjson/(.*)', UploadJSONResponse),
+                                      ('/return_texts', ReturnTexts),
                                       ],
                                      debug=True)
 
--- appengine/handleInput.py
+++ appengine/handleInput.py
@@ -1,13 +1,43 @@
-import xml
+import logging
 import re
+import xml
 from xml.dom import minidom
 
 TEI_NS = 'http://www.tei-c.org/ns/1.0'
 ELEMENT_NODE = xml.dom.Node.ELEMENT_NODE
 TEXT_NODE = xml.dom.Node.TEXT_NODE
 
+def parse_file( content ):
+    '''Takes a string that is the contents of a file, and determines the
+    individual texts within it.  ID is of the form X-Y-Z, dependent on
+    number of levels of subtexts.'''
+    try:
+        type = filetype( content )
+    except xml.parsers.expat.ExpatError:
+        raise  ## It was an XML document but it failed to parse
+    else:
+        ## Either it parsed, or it wasn't an XML doc at all.
+        textlist = []
+        if( type == 'plaintext' ):
+            textlist.append( { 'offset': 0,
+                               'length': len( content ),
+                               'parent': None,
+                               'type': type,
+                               'id': '0',
+                               } )
+        elif( type == 'teixml' ):
+            textlist = find_tei_texts( content )
+        elif( type == 'collatexinput' ):
+            textlist = find_collatex_witnesses( content )
+        else:
+            logging.error( 'Do not support file type %s' % filetype )
+            raise UnsupportedFiletypeException
+    return textlist
+    
+
 def filetype( xmlstr ):
-    '''Takes an XML string and looks at its contents to make a guess as to how it should be tokenized.'''
+    '''Takes an XML string and looks at its contents to make a guess as to
+    how it should be tokenized.'''
     type = 'unknown'
     try:
         xmlfile = minidom.parseString( xmlstr )
@@ -15,122 +45,85 @@ def filetype( xmlstr ):
         if( xmlfile.documentElement.getAttribute( 'xmlns' ) == TEI_NS ):
             type = 'teixml'
         # Add here any other types of file we learn how to parse.
+        elif( xmlfile.documentElement.nodeName() == 'example' ):
+            # it is a CollateX lightweight witness file
+            type = 'collatexinput'
         else:
             type = 'otherxml'
         xmlfile.unlink()
     except xml.parsers.expat.ExpatError, inst:
-        # Assume it should be tokenized as plain text.
         if( re.match( 'syntax error: line \d+, column 0', inst.args[0] ) ):
+            # Assume it should be tokenized as plain text.
             type = 'plaintext'
         else:
+            # It was XML but failed to parse for some other reason.
             raise
     return( type )
+
+def find_tei_texts( teistr ):
+    '''Returns the set of available texts in a TEI-encoded file.'''
+    teiElement = get_tei_document_node( teistr )
+    ## The passed teiElement could either be a teiCorpus with TEI children
+    ## or a single TEI.  We need to operate on TEI nodes.
+    teinodes = []
+    logging.info( "Got doc element with name %s" % teiElement.nodeName )
+    if( teiElement.nodeName == 'teiCorpus' ):
+        teinodes = teiElement.getElementsByTagName( 'TEI' )
+    else:
+        teinodes = [ teiElement ]
+    
+    textlist = []
+    for teinode in teinodes:
+        ## Should return a dict of offset, length, id.
+        sequence = len( textlist )
+        topchildren = teinode.childNodes
+        for c in topchildren:
+            if( c.nodeType == ELEMENT_NODE and c.localName == 'text' ):
+                childinfo = {}
+                childstr = c.toxml( 'utf-8' )
+                childid = str( sequence )
+                childinfo['offset'] = teistr.find( childstr )
+                childinfo['length'] = len( childstr )
+                childinfo['type'] = 'teixml'
+                childinfo['id'] = childid
+                textlist.append( childinfo )
+    return textlist
 
 def get_tei_document_node( xmlstr ):
     '''Takes a file or a filehandle and returns a list of the elements
     therein that are named TEI.  This makes a good starting point for
-    find_text_children.'''
-    teifile = minidom.parseString( xmlstr.encode( "utf-8" ))
+    find_tei_texts.'''
+    teifile = minidom.parseString( xmlstr )
     if( teifile.documentElement.tagName.lower() == 'teicorpus' or
+        teifile.documentElement.tagName.lower() == 'tei.2' or
         teifile.documentElement.tagName.lower() == 'tei' ):
         return teifile.documentElement
     else:
         raise NoTEIDocumentElement( 'Document element is ' 
                                     + teifile.documentElement.tagName )
 
-def get_text( xmlstr, textaddr ):
-    '''Takes an XML string and an address for a particular <text/> node
-    within the XML.  Returns the text node as a string. TODO minimize
-    calls to find_text_children.'''
-    teifile = minidom.parseString( xmlstr.encode( "utf-8" ))
-    children = find_text_children( teifile.documentElement )
-    address = textaddr.split( '-' )
-    currtext = None
-    for bit in address:
-        currtext = children[int( bit )]
-        children = currtext.subtexts
-    # At this point currtext should hold the text we are after.
-    return currtext.xmlobj.toxml()
+def find_witnesses( xmlstr ):
+    doc = minidom.parsestring( xmlstr )
+    examples = doc.documentElement.getElementsByTagName( 'example' )
+    sequence = 0
+    textlist = []
+    for ex in examples:
+        witnesses = ex.getElementsByTagName( 'witness' )
+        for wit in witnesses:
+            witstr = wit.toxml( 'utf-8' )
+            wit_id = wit.getAttribute( 'id' )
+            if sequence:
+                wit_id = wit_id + '-' + str( sequence )
+            wit_info = { 'id': wit_id,
+                         'offset': xmlstr.find( witstr ),
+                         'length': len( witstr ),
+                         'parent': None,   # collatex input doesn't have subtexts
+                         'type': 'collatexinput' }
+            textlist.append( wit_info )
+        sequence += 1
+    return textlist
 
-
-def find_text_children( teiElement ):
-    '''Returns the set of available texts in a TEI-encoded file.'''
-    alltextnodes = []
-    ## The passed teiElement could either be a teiCorpus with TEI children
-    ## or a single TEI.  We need to operate on TEI nodes.
-    teinodes = []
-    # Lowercasing these tags is a HORRIBLE HACK and should die SOON
-    if( teiElement.tagName == 'teicorpus' ):
-        teinodes = teiElement.getElementsByTagName( 'tei' )
-    else:
-        teinodes = [ teiElement ]
-    for teinode in teinodes:
-        ## Should return a tuple of textElement, preview, children
-        topchildren = teinode.childNodes
-        for c in topchildren:
-            # Recurse to find text descendants.
-            if( c.nodeType == ELEMENT_NODE and c.localName == 'text' ):
-                grandchildren = []
-                preview = ''
-                frontmatter = c.getElementsByTagName( 'front' )
-                if( len( frontmatter ) ):
-                    preview = string_from_wordlist( get_textnode_words( frontmatter[0] ), 100 )
-                    preview += ' // '
-                body = c.getElementsByTagName( 'body' )
-                if( len( body ) ):
-                    preview += string_from_wordlist( get_textnode_words( body[0] ), 100 )
-                for cg in c.childNodes:
-                    if( cg.nodeType == ELEMENT_NODE and cg.localName == 'group' ):
-                        grandchildren.extend( find_text_children( cg ) )
-                textObj = teiText( c, preview, grandchildren )
-                alltextnodes.append( textObj )
-    return alltextnodes
-
-def get_textnode_words( element ):
-    '''A utility function to make up for lack of XPath.  Join together all the
-    text-node descendants into a single list of words.'''
-    out_words = []
-    for child in element.childNodes:
-        if( child.nodeType == TEXT_NODE ):
-            content = child.data.strip()
-            out_words.extend( content.split() )
-        elif( child.hasChildNodes ):
-            out_words.extend( get_textnode_words( child ) )
-    return out_words
-
-def string_from_wordlist( wl, length ):
-    '''An even dumber utility function.  Return a concatenated string of words
-    up to the maximum specified in length.'''
-    out_str = wl[0]; del wl[0]
-    for word in wl:
-        if( len( out_str ) < length ):
-            out_str += ' ' + word
-        else:
-            break
-    return out_str
-
-class teiText:
-    def __init__( self, xmlobj, preview, children = None ):
-        self.xmlobj = xmlobj
-        self.preview = preview
-        self.subtexts = []
-        if( children is not None ):
-            self.add_subtexts( children )
-    def add_subtexts( self, children ):
-        for c in children:
-            if c.__class__.__name__ is not 'teiText':
-                raise WrongObjectError
-            self.subtexts.append( c )
-    def dict_export ( self ):
-        '''Will only export the XML text string and the subtexts.'''
-        data_struct = { 'content' : self.xmlobj.toxml(),
-                        'type': 'teixml' }
-        if( len( self.subtexts ) > 0 ):
-            data_struct[ 'subtexts' ] = []
-            for st in self.subtexts:
-                data_struct[ 'subtexts' ].append( st.dict_export() )
-        return data_struct
-    
 
 class WrongObjectError( Exception ): pass
 class NoTEIDocumentElement( Exception ): pass
+class UnsupportedFiletypeException( Exception ): pass
