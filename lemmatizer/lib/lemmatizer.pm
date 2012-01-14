8101ee7418d30b3c77d6af4485e92333d13b5c75
Tara L Andrews <tla\@mit.edu>
2012-01-14 22:34:04 +0100

    add John verse as default graph

--- lemmatizer/lib/lemmatizer.pm
+++ lemmatizer/lib/lemmatizer.pm
@@ -16,6 +16,7 @@ use Catalyst::Runtime 5.80;
 use parent qw/Catalyst/;
 use Catalyst qw/-Debug
                 ConfigLoader
+                Unicode::Encoding
                 Static::Simple/;
 our $VERSION = '0.01';
 
--- lemmatizer/lib/lemmatizer/Controller/Root.pm
+++ lemmatizer/lib/lemmatizer/Controller/Root.pm
@@ -28,152 +28,146 @@ lemmatizer::Controller::Root - Root Controller for lemmatizer
 
 =cut
 
-my $graph;
+my $tradition;
 
 sub index :Path :Args(0) {
-    my ( $self, $c ) = @_;
+	my ( $self, $c ) = @_;
 
-    # First we need to generate the SVG from the GraphML, and also keep track
-    # of the GraphML nodes.
+	# First we need to generate the SVG from the GraphML, and also keep track
+	# of the GraphML nodes.
 
-    # Hacky hacky to make this work with the interedition-tools interface:
-    # accept XML in a parameter called 'result'.
-    my $origin_data = $c->request->params->{'result'};
-    unless( $origin_data ) {	
-	my $dummy_file = $c->path_to( 't', 'data', 'Collatex-16.xml' );
-	open( GRAPHFILE, $dummy_file ) or die "Could not open $dummy_file";
-	my @lines = <GRAPHFILE>;
-	close GRAPHFILE;
-	$origin_data = join( '', @lines );
-    }
-    my $tradition = Text::Tradition->new( 
-	'CollateX' => $origin_data,
-	'name' => 'Collatex_16', );
-    $graph = $tradition->collation;
-    my $svg_str = $graph->as_svg;
-    $svg_str =~ s/\n//gs;
-    $c->stash->{svg_string} = $svg_str;
-    my @initial_nodes = $graph->lemma_readings();
-    $c->stash->{initial_text} = join( ' ', map { $_->[1] ? $graph->reading( $_->[0] )->label : '...' } @initial_nodes );
-    $c->stash->{template} = 'testsvg.tt2';
+	# Hacky hacky to make this work with the interedition-tools interface:
+	# accept XML in a parameter called 'result'.
+	my $datatype = $c->request->param('type') || 'Self';
+	my $informat = $c->request->param('format') || 'string';
+	my $input = $c->request->param('result');
+	unless( $input ) {
+		# Default testing stuff.
+		$informat = 'file';
+		$input = $c->path_to( 't', 'data', 'john.xml' );
+	}
+	$tradition = Text::Tradition->new( 
+    	'name'  => 'inline', 
+    	'input' => $datatype,
+    	$informat  => $input,
+    	);
+	my $collation = $tradition->collation;
+	my $svg_str = $collation->as_svg;
+	$svg_str =~ s/\n//gs;
+	$c->stash->{'svg_string'} = $svg_str;
+	$c->stash->{'template'} = 'testsvg.tt2';
 }
 
 # Utility function to render SVG from a graph input.
 sub renderSVG :Global {
-    my( $self, $c ) = @_;
-    my $origin_data = $c->request->params->{'data'};
-    my $type = $c->request->params->{'type'};
-    my $tradition = Text::Tradition->new( $type => $origin_data );
-    my $svg_str = $tradition->collation->as_svg;
-    $c->response->content_type( 'application/svg+xml' );
-    $c->response->content_encoding( 'UTF-8' );
-    $c->response->body( $svg_str );
-}
-
-sub node_click :Global {
-    my ( $self, $c ) = @_;
-    my $node = $c->request->params->{'node_id'};
-
-    my @off = $graph->toggle_reading( $node );
-    my @active = $graph->lemma_readings( @off );
-
-    $c->response->content_type( 'application/json' );
-    $c->response->content_encoding( 'UTF-8' );
-    $c->response->body( encode_json( \@active ) );
+	my( $self, $c ) = @_;
+	my $origin_data = $c->request->param('data');
+	my $format = $c->request->param('format') || 'string';
+	my $type = $c->request->param('type');
+	my $name = $c->request->param('name') || 'Collation graph';
+	my $tradition = Text::Tradition->new( 
+		'name' => $name,
+		'input' => $type,
+		$format => $origin_data,
+		);
+	$c->stash->{'result'} = $tradition->collation->as_svg;
+	$c->forward('View::SVG');
 }
 
 sub node_collapse :Global {
-    my( $self, $c ) = @_;
+	my( $self, $c ) = @_;
+	my $collation = $tradition->collation;
 
-    my $node = $c->request->params->{'source_id'};
-    my $target = $c->request->params->{'target_id'};
-    my $reason = $c->request->params->{'reason'};
-    my $global = $c->request->params->{'global'};
-    $global = 1 if $global; # make it a Bool for Moose
-    my $response = {};
+	my $node = $c->request->param('source_id');
+	my $target = $c->request->param('target_id');
+	my $relation = $c->request->param('reason');
+	my $note = $c->request->param('note');
+	my $scope = $c->request->param('scope'); # TODO change to scope
+	my $response = {};
 
-    # TODO all the work.  For now hardcode a test case.
-    my $opts = { 'type' => $reason,
-		 'global' => $global };
-    my( $status, @nodes ) = $graph->add_relationship( $node, $target, $opts );
+	# TODO all the work.  For now hardcode a test case.
+	my $opts = { 'type' => $relation,
+				 'scope' => $scope };
+	my( $status, @vectors ) = $collation->add_relationship( $node, $target, $opts );
 
-    $c->response->status( $status ? 200 : 403 );
+	$c->response->status( $status ? 200 : 403 );
 
-    if( $status ) {
-	# TODO work
-	foreach my $pair ( @nodes ) {
-	    my( $ps, $pt ) = @$pair;
-	    $response->{$ps} = { 'target' => $pt };
+	if( $status ) {
+		# TODO work
+		foreach my $pair ( @vectors ) {
+			my( $ps, $pt ) = @$pair;
+			$response->{$ps} = { 'target' => $pt };
+		}
+
+		foreach my $n ( keys %$response ) {
+			# Edges are of the form $node . &#45;&gt; . $node
+			# Look for any predecessor and successor node shared
+			# between source and target, and collapse those edges.
+			# We want response->node->target = (node)
+			#						->edges->target = (edge)
+			#							   ->label = (label)
+			my $collapsed_edges = find_dup_edges( $n, $response->{$n}->{'target'} );
+			$response->{$n}->{'edges'} = $collapsed_edges;
+		}
 	}
-
-	foreach my $n ( keys %$response ) {
-	    # Edges are of the form $node . &#45;&gt; . $node
-	    # Look for any predecessor and successor node shared
-	    # between source and target, and collapse those edges.
-	    # We want response->node->target = (node)
-	    #                       ->edges->target = (edge)
-	    #                              ->label = (label)
-	    my $collapsed_edges = find_dup_edges( $n, $response->{$n}->{'target'} );
-	    $response->{$n}->{'edges'} = $collapsed_edges;
-	}
-    }
-    $c->response->content_type( 'application/json' );
-    $c->response->content_encoding( 'UTF-8' );
-    $c->response->body( encode_json( $response ) );
+	$c->response->content_type( 'application/json' );
+	$c->response->content_encoding( 'UTF-8' );
+	$c->response->body( encode_json( $response ) );
 }
 
 sub find_dup_edges {
-    my( $source, $target ) = @_;
-    $graph->collapse_graph_paths();
-    my @source_origin = map { $_->from } $graph->reading( $source )->incoming();
-    my @target_origin = map { $_->from } $graph->reading( $target )->incoming();
-    my @source_dest = map { $_->to } $graph->reading( $source )->outgoing();
-    my @target_dest = map { $_->to } $graph->reading( $target )->outgoing();
-    my @shared_origin = union( \@source_origin, \@target_origin );
-    my @shared_dest = union( \@source_dest, \@target_dest );
-    my $result = {};
-    foreach my $n ( @shared_origin ) {
-	# This is a hardcoded hack that will break if GraphViz changes its
-	# SVG rendering logic.
-	my $source_svg_id = $n->name . '&#45;&gt;' . $source;
-	my $target_svg_id = $n->name . '&#45;&gt;' . $target;
-	# There is only one of these.
-	my @el = $n->edges_to( $graph->reading( $source) );
-	my $edgelabel = join( ', ', '', $el[0]->name );
-	$result->{$source_svg_id} = { 'target' => $target_svg_id,
-				      'label'  => $edgelabel };
-    }
-    foreach my $n ( @shared_dest ) {
-	# This is a hardcoded hack that will break if GraphViz changes its
-	# SVG rendering logic.
-	my $source_svg_id = $source . '&#45;&gt;' . $n->name;
-	my $target_svg_id = $target . '&#45;&gt;' . $n->name;
-	# There is only one of these.
-	my @el = $graph->reading( $source )->edges_to( $n );
-	my $edgelabel = join( ', ', '', $el[0]->name );
-	$result->{$source_svg_id} = { 'target' => $target_svg_id,
-				      'label'  => $edgelabel };
-    }
-    $graph->expand_graph_paths();
-    return $result;
-}
-sub union {
-    my( $list1, $list2 ) = @_;
-    my %all;
-    my @union;
-    map { $all{$_->name} = 1 } @$list1;
-    foreach my $l ( @$list2 ) {
-	if( $all{$l->name} ) {
-	    push( @union, $l );
+	my( $source, $target ) = @_;
+	my $collation = $tradition->collation;
+	my @source_origin = $collation->reading( $source )->predecessors();
+	my @target_origin = $collation->reading( $target )->predecessors();
+	my @source_dest = $collation->reading( $source )->successors();
+	my @target_dest = $collation->reading( $target )->successors();
+	my @shared_origin = union( \@source_origin, \@target_origin );
+	my @shared_dest = union( \@source_dest, \@target_dest );
+	my $result = {};
+	$DB::single = 1;
+	foreach my $n ( @shared_origin ) {
+		# This is a hardcoded hack that will break if GraphViz changes its
+		# SVG rendering logic.
+		my $source_svg_id = $n->id . '&#45;&gt;' . $source;
+		my $target_svg_id = $n->id . '&#45;&gt;' . $target;
+		# There is only one of these.
+		my $el = $collation->path_display_label( [ $n->id, $source ] );
+		my $edgelabel = join( ', ', '', $el );
+		$result->{$source_svg_id} = { 'target' => $target_svg_id,
+									  'label'  => $edgelabel };
 	}
-    }
-    return @union;
+	foreach my $n ( @shared_dest ) {
+		# This is a hardcoded hack that will break if GraphViz changes its
+		# SVG rendering logic.
+		my $source_svg_id = $source . '&#45;&gt;' . $n->id;
+		my $target_svg_id = $target . '&#45;&gt;' . $n->id;
+		# There is only one of these.
+		my $el = $collation->path_display_label( [ $source, $n->id ] );
+		my $edgelabel = join( ', ', '', $el );
+		$result->{$source_svg_id} = { 'target' => $target_svg_id,
+									  'label'  => $edgelabel };
+	}
+	return $result;
+}
+
+sub union {
+	my( $list1, $list2 ) = @_;
+	my %all;
+	my @union;
+	map { $all{$_->id} = 1 } @$list1;
+	foreach my $l ( @$list2 ) {
+		if( $all{$l->id} ) {
+			push( @union, $l );
+		}
+	}
+	return @union;
 }
 
 sub default :Path {
-    my ( $self, $c ) = @_;
-    $c->response->body( 'Page not found' );
-    $c->response->status(404);
+	my ( $self, $c ) = @_;
+	$c->response->body( 'Page not found' );
+	$c->response->status(404);
 }
 
 =head2 end
